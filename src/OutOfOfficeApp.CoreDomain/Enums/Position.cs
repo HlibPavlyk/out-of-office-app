6175bc7a163fadadf6686291338cb1d0cce78ac6
HlibPavlyk <pavlikgo78@gmail.com>
2024-06-19 09:35:44 +0300

    feat: add enums for employee

---
+++ src/OutOfOfficeApp.CoreDomain/Enums/Position.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOfficeApp.CoreDomain.Enums
+{
+    public enum Position
+    {
+        Employee,
+        HRManager,
+        ProjectManager,
+        Administrator
+    }
+}
