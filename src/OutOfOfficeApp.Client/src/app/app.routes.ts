998ea35c1e6bc3ee88dd84839c83324a202214ec
HlibPavlyk <pavlikgo78@gmail.com>
2024-06-29 21:07:19 +0300

    feat: add auth guard on client site routes

--- src/OutOfOfficeApp.Client/src/app/app.routes.ts
+++ src/OutOfOfficeApp.Client/src/app/app.routes.ts
@@ -2,10 +2,15 @@ import { Routes } from '@angular/router';
 import {AddEmployeeComponent} from "./employee/add-employee/add-employee.component";
 import {EmployeeComponent} from "./employee/employee.component";
 import {LoginComponent} from "./login/login.component";
+import {authGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/employees', pathMatch: 'full' },
-  { path: 'employees', component: EmployeeComponent },
+  {
+    path: 'employees',
+    component: EmployeeComponent,
+    canActivate: [authGuard]
+  },
   { path: 'add-employee', component: AddEmployeeComponent },
   { path: 'edit-employee/:id', component: AddEmployeeComponent },
   { path: 'login', component: LoginComponent },
