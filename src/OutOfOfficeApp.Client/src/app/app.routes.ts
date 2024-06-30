421473928564b98430fd1ad049ead6547e93fcef
HlibPavlyk <pavlikgo78@gmail.com>
2024-06-30 19:50:53 +0300

    feat: add welcome page

--- src/OutOfOfficeApp.Client/src/app/app.routes.ts
+++ src/OutOfOfficeApp.Client/src/app/app.routes.ts
@@ -3,14 +3,16 @@ import {AddEmployeeComponent} from "./employee/add-employee/add-employee.compone
 import {EmployeeComponent} from "./employee/employee.component";
 import {LoginComponent} from "./login/login.component";
 import {authGuard} from "./guards/auth.guard";
+import {HomeComponent} from "./home/home.component";
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/employees', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'employees',
     component: EmployeeComponent,
     canActivate: [authGuard]
   },
+  { path: 'home', component: HomeComponent },
   { path: 'add-employee', component: AddEmployeeComponent },
   { path: 'edit-employee/:id', component: AddEmployeeComponent },
   { path: 'login', component: LoginComponent },
